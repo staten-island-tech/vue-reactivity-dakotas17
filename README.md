4d7d9279420127ef200860764ef3bd749783222e
dakotas17 <dakotas17@nycstudents.net>
2024-02-07 09:24:04 -0500

    awdeae

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-7f7980b617ed060a017424585567c406b6ee15c891e84e1186181d67ecf80aa0.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=13751362)
+# vite-project
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+<DestCard v-for="destination in destinations" :key="destination.name" :Destination="destination"
+
+/>
+
+  </div>
+</template>
+
+
+<script setup>
+import DestCard from "@/components/DestCard.vue";
+const destinations = [{
+          name: "Chocolate Round",
+          International: true,
+          price: 1300,
+          img: "https://tutti-dolci.com/wp-content/uploads/2018/08/Classic-Chocolate-Layer-Cake-1-copy.jpg",
+        },
+        {
+          name: "Chocolate Square",
+          International: true,
+          price: 800,
+          img: "https://ne-mos-bakery.myshopify.com/cdn/shop/products/HFG_ChocCake_Sq_1024x1024@2x.jpg?v=1596492671",
+        },
+        {
+          name: "Vanilla Round",
+          International: true,
+          price: 1200,
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQELfv4I6rNrFtU5vLiCcvAdLh-dD69SNal9Q&usqp=CAU",
+        },
+        {
+          name: "Vanilla Square",
+          International: false,
+          price: 900,
+          img: "https://sallysbakingaddiction.com/wp-content/uploads/2019/06/vanilla-sheet-cake.jpg",
+        },
+        {
+          name: "Cascade Mountains",
+          International: false,
+          price: 400,
+          img: "https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80&quot",
+        },
+        {
+          name: "Egyptian Exploration",
+          International: true,
+          price: 800,
+          img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot",
+        },
+      ]
+</script>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>

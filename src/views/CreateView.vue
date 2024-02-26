79f0426baca5d334c47b096a9f4840c25e5ce793
dakotas17 <dakotas17@nycstudents.net>
2024-02-26 09:24:01 -0500

    jdrdg

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -5,28 +5,36 @@
     <h1>Design Your Own Cake!</h1>
 
     <h2>Base</h2>
-    <button>vanilla</button>
-    <button>chocolate</button>
+    <button v-bind:class="{vc}">vanilla</button>
+    <button v-bind:class="{cc}">chocolate</button>
 
     <h2>Frosting</h2>
-    <button>vanilla</button>
-    <button>chocolate</button>
-    <button>strawberry</button>
+    <button v-bind:class="{vf}">vanilla</button>
+    <button v-bind:class="{cf}">chocolate</button>
+    <button v-bind:class="{sf}">strawberry</button>
 
     <h2>Sprinkles</h2>
-    <button>option 1</button>
+    <button v-bind:class="{sprinkles}">option 1</button>
 
     <h2>Candles</h2>
-    <button>option 1</button>
-    <button>option 2</button>
+    <button v-bind:class="{candle1}">option 1</button>
+    <button v-bind:class="{candle2}">option 2</button>
 
     <h2>Shape</h2>
-    <button>round</button>
-    <button>square</button>
+    <button v-bind:class="{round}">round</button>
+    <button v-bind:class="{square}">square</button>
   </div>
+
+  <h1>Total Ingredients</h1>
+
+
+
+
+
 </template>
 
 <script setup>
+
 
 </script>
 
